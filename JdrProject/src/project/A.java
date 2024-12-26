1d34668601f69301cafa24b7ab17e8c40b04ff02
quan2 <quan2@BOOK-8B9E0K09Q7>
2024-12-26 22:42:29 +0900

    change A and B in devB Branch

--- JdrProject/src/project/A.java
+++ JdrProject/src/project/A.java
@@ -3,10 +3,12 @@ package project;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("A is Main Class");
+		System.out.println("devB branch change A");
 		B Hello2 = new B();
 		C Hello3 = new C();
 		Hello2.Hello_B();
 		Hello3.Hello_C();
+		
 		
 	}
 
